f19b46bc74666c43cde7da7e37c2587c34b9f0fd
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2022-09-19 19:23:14 +0530

    fixed some styling

--- src/containers/Table/index.jsx
+++ src/containers/Table/index.jsx
@@ -24,60 +24,71 @@ const Table = ({ headers, tollsPage, filters, searchText }) => {
 
     let data = [];
     if (tollsPage) {
-        data = Object.keys(tolls).map((tollName, index) => {
-            if (
-                searchText === "" ||
+        data = Object.keys(tolls);
+        if (searchText !== "")
+            data = data.filter((tollName) =>
                 tollName.toLowerCase().includes(searchText.toLowerCase())
-            )
-                return (
-                    <tr key={index}>
-                        <td>{tollName}</td>
-                        <td>
-                            {tolls[tollName].tarrifs.carSingle}/
-                            {tolls[tollName].tarrifs.carReturn}
-                        </td>
-                        <td>
-                            {tolls[tollName].tarrifs.lcvSingle}/
-                            {tolls[tollName].tarrifs.lcvReturn}
-                        </td>
-                        <td>
-                            {tolls[tollName].tarrifs.truckSingle}/
-                            {tolls[tollName].tarrifs.truckReturn}
-                        </td>
-                        <td>
-                            {tolls[tollName].tarrifs.heavySingle}/
-                            {tolls[tollName].tarrifs.heavyReturn}
-                        </td>
-                        <td
-                            className="delteButton"
-                            onClick={() => {
-                                dispatch(tollsActions.deleteToll(tollName));
-                            }}
-                        >
-                            <RiDeleteBin5Line />
-                        </td>
-                    </tr>
-                );
-                return null;
+            );
+        data = data.map((tollName, index) => {
+            return (
+                <tr key={index}>
+                    <td>{tollName}</td>
+                    <td>
+                        {tolls[tollName].tarrifs.carSingle}/
+                        {tolls[tollName].tarrifs.carReturn}
+                    </td>
+                    <td>
+                        {tolls[tollName].tarrifs.lcvSingle}/
+                        {tolls[tollName].tarrifs.lcvReturn}
+                    </td>
+                    <td>
+                        {tolls[tollName].tarrifs.truckSingle}/
+                        {tolls[tollName].tarrifs.truckReturn}
+                    </td>
+                    <td>
+                        {tolls[tollName].tarrifs.heavySingle}/
+                        {tolls[tollName].tarrifs.heavyReturn}
+                    </td>
+                    <td
+                        className="delteButton"
+                        onClick={() => {
+                            dispatch(tollsActions.deleteToll(tollName));
+                        }}
+                    >
+                        <RiDeleteBin5Line />
+                    </td>
+                </tr>
+            );
         });
     } else {
-        data = logs.map((row, index) => {
-            if (
-                (filters.length === 0 || filters.includes(row.tollName)) &&
-                (searchText === "" ||
-                    row.vehicleNumber.toLowerCase().includes(searchText.toLowerCase()))
-            )
-                return (
-                    <tr key={index}>
-                        <td>{mappings[row.vehicleType]}</td>
-                        <td>{row.vehicleNumber}</td>
-                        <td>{new Date(row.dateTime).toLocaleString()}</td>
-                        <td>{row.tollName}</td>
-                        <td>{row.tarrif}</td>
-                        <span></span>
-                    </tr>
-                );
-            return null;
+        // if (
+        //     (filters.length === 0 || filters.includes(row.tollName)) &&
+        //     (searchText === "" ||
+        //         row.vehicleNumber.toLowerCase().includes(searchText.toLowerCase()))
+        // )
+        data = logs;
+        // console.log(data)
+        if (searchText !== "") {
+            data = data.filter((log) =>
+                log.vehicleNumber
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase())
+            );
+        }
+        if (filters.length !== 0) {
+            data = data.filter((log) => filters.includes(log.tollName));
+        }
+        data = data.map((row, index) => {
+            return (
+                <tr key={index}>
+                    <td>{mappings[row.vehicleType]}</td>
+                    <td>{row.vehicleNumber}</td>
+                    <td>{new Date(row.dateTime).toLocaleString()}</td>
+                    <td>{row.tollName}</td>
+                    <td>{row.tarrif}</td>
+                    <span></span>
+                </tr>
+            );
         });
     }
     return (
@@ -85,7 +96,24 @@ const Table = ({ headers, tollsPage, filters, searchText }) => {
             <thead>
                 <tr>{headersRender}</tr>
             </thead>
-            <tbody>{data}</tbody>
+            <tbody>
+                {data.length !== 0 ? (
+                    data
+                ) : (
+                    <div
+                        style={{
+                            textAlign: "center",
+                            position: "absolute",
+                            top: "20rem",
+                            left: "50%",
+                            transform: "translateX(-50%)",
+                            fontSize: "1.5rem",
+                        }}
+                    >
+                        Not Found
+                    </div>
+                )}
+            </tbody>
         </table>
     );
 };
